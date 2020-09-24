e159efe3551324491b760f07d5381f76da21a6e4
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-09-24 15:33:15 -0500

    feat: killin it

--- js/app.js
+++ js/app.js
@@ -1,44 +1,24 @@
 const url = 'https://fabulous-difficult-redcurrant.glitch.me/movies';
+const omdb = `http://www.omdbapi.com/?apikey=${MDBKey}&`
 let IDToEdit;
-let newRating;
+let rating;
 
-function movieFetch () {
-    fetch(url).then(response => response.json())
-        .then( data => {
-            $("#movie-list").empty();
-            let list = ""
-            for (let item of data){
-                let rating = item.rating;
-                list +=  `<div class="movie-container" id="${item.id}">
-                            <h4>${item.title}</h4>
-                            <div class="movieRating">${rating}</div><br>
-                            <button class="btn btn-primary edit" type="button" data-toggle="modal" data-target="#editModal">Edit Rating</button>
-                            <button class="btn btn-danger delete">Delete</button>
-                            <hr>
-                            </div>`;
-            };
-            $("#movie-list").append(list);
-        })
-        .catch(error => console.error(error));
-};
+const starterMovies = ['Nausicaä of the Valley of the Wind ', 'Blade Runner', 'Spaceballs', 'Rubber']
+// const omdbFetch = (movie) => fetch(omdb).then(response => response.json());
 
+function movieEntry(movieTitle) {
+        fetch(`${omdb}t=${movieTitle}`).then(response => response.json()).then(data => {
+            console.log(data);
+            return data;
+        });
+}
 
-function addMovieFunction () {
-    let newMovieID;
-    fetch(url).then(response => response.json()).then(data => data.reduce((total,movie) => {
-        if(movie.id + 1 > total){
-            return movie.id + 1;
-        };
-        console.log(data);
-        return  newMovieID = total;
-    },0))
-    let newTitle = $("#newMovieTitle").val();
-    let newRating = $('input[name="rating"]:checked').val();
+function omdbMovieFunction (movie) {
     const reviewObj = {
-        id: newMovieID,
-        title: newTitle,
-        rating: newRating,
-        // comments: "This is a really good place for coding and eating"
+        title: movie.Title,
+        rating: '',
+        genre: movie.Genre,
+        poster: movie.Poster,
     };
 
     const options = {
@@ -52,6 +32,67 @@ function addMovieFunction () {
         .then(response => console.log(response))
         .then(movieFetch)/* review was created successfully */
         .catch(error => console.error(error)); /* handle errors */
+};
+
+function movieFetch () {
+    fetch(url).then(response => response.json())
+        .then( data => {
+            $("#movie-list").empty();
+            let list = ""
+            for (let item of data){
+                let rating = item.rating;
+                list +=  `<div class="movie-container" id="${item.id}">
+                            <h4>${item.title}</h4>
+                            <img src="${item.poster}">
+                            <div class="movieRating">Your rating is 
+                                <span class="rating">${rating}</span>
+                                ⭐️ out of 5⭐️ 
+                            </div>
+                            <br>
+                            <button class="btn btn-primary edit" type="button" data-toggle="modal" data-target="#editModal">Edit Rating</button>
+                            <button class="btn btn-danger delete">Delete</button>
+                            <hr>
+                            </div>`;
+            };
+            $("#movie-list").append(list);
+        })
+        .catch(error => console.error(error));
+};
+
+function addMovieFunction (movie) {
+    let newTitle = $("#newMovieTitle").val();
+    let newRating = $('input[name="rating"]:checked').val();
+    fetch(`${omdb}t=${newTitle}`).then(response => response.json()).then(movie => {
+        console.log(movie);
+        const reviewObj = {
+            title: movie.Title,
+            rating: newRating,
+            poster: movie.Poster,
+            rated: movie.Rated,
+            year: movie.Year,
+            director: movie.Director,
+            writer: movie.Writer,
+            genre: movie.Genre,
+            runtime: movie.Runtime,
+            awards: movie.Awards,
+            plot: movie.Plot,
+            ratings: movie.Ratings
+        };
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(reviewObj),
+        };
+        fetch(url, options)
+            .then(response => console.log(response))
+            .then(movieFetch)/* review was created successfully */
+            .catch(error => console.error(error)); /* handle errors */
+
+    });
+
 };
 
 function moviePatch (editRating) {
@@ -79,7 +120,10 @@ const movieDelete = (id) => {
 };
 
 $(document).ready(function() {
-    movieFetch();
+    setTimeout(function () {
+        movieFetch();
+    },2500);
+
 });
 
 $("#addMovieButton").click(() => {
@@ -88,15 +132,17 @@ $("#addMovieButton").click(() => {
 
 $("#addMovie").click(() => {
     if($("#newMovieTitle").val().trim().length > 0) {
+        // let entry = movieEntry($("#newMovieTitle").val().trim());
         addMovieFunction();
+
     } else {
         alert("Hey, Can't do that!")
-    }
+    };
 });
 
 $(document).on("click", ".edit", function() {
     IDToEdit = $(this).parent().attr('id');
-    let currentRating = $(this).parent().children().first().next().html()
+    let currentRating = $(this).parent().find('.rating').html();
     console.log(currentRating);
     $(`input[name='editRating'][value=${currentRating}]`).prop('checked', true);
     console.log(IDToEdit);
