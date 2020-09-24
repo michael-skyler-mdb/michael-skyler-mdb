1a08325f97d6d82b9c7ac74b8459b633a53c597f
Michael Meyer <michael.c.meyer26@gmail.com>
2020-09-23 20:08:02 -0500

    Fixed Loading Screen, reformatted Add and Edit to use modals, started initial implementaion of search functions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const url = 'https://fabulous-difficult-redcurrant.glitch.me/movies';
+let IDToEdit;
 
 function movieFetch () {
     fetch(url).then(response => response.json())
@@ -8,32 +9,11 @@ function movieFetch () {
             for (let item of data){
                 let rating = item.rating;
                 list +=  `<div class="movie-container" id="${item.id}">
-                            <h5>${item.title}</h5>
+                            <h4>${item.title}</h4>
                             <div>${rating}</div><br>
-                            <button class="btn btn-primary edit" type="button" data-toggle="collapse" data-target="#editForm${item.id}" aria-expanded="false" aria-controls="collapseExample">Edit Rating</button>
+                            <button class="btn btn-primary edit" type="button" data-toggle="modal" data-target="#editModal">Edit Rating</button>
                             <button class="btn btn-danger delete">Delete</button>
-                            <div class="collapse" id="editForm${item.id}">
-                            <div class="card card-body">
-                                <div id="edit-form">
-                            <label>
-                                <input type="radio" name="editRating${item.id}" value="1" checked>1
-                            </label>
-                            <label>
-                                <input type="radio" name="editRating${item.id}" value="2">2
-                            </label>
-                            <label>
-                                <input type="radio" name="editRating${item.id}" value="3">3
-                            </label>
-                            <label>
-                                <input type="radio" name="editRating${item.id}" value="4">4
-                            </label>
-                            <label>
-                                <input type="radio"name="editRating${item.id}" value="5">5
-                            </label>
-                            <button class="btn btn-success submit-edit" id="editMovie${item.id}">Submit</button>
-                            </div>
-                            </div>
-                           </div>
+                            <hr>
                             </div>`;
                 // $(`input:nth-child(${rating})`);
             };
@@ -74,7 +54,7 @@ function addMovieFunction () {
         .catch(error => console.error(error)); /* handle errors */
 };
 
-function moviePatch (patchID, editRating) {
+function moviePatch (editRating) {
     const reviewObj = {
         rating: editRating
     };
@@ -86,7 +66,7 @@ function moviePatch (patchID, editRating) {
         },
         body: JSON.stringify(reviewObj),
     };
-    fetch(`${url}/${patchID}`, options)
+    fetch(`${url}/${IDToEdit}`, options)
         .then(response => console.log(response))
         .then(movieFetch)/* edit was created successfully */
         .catch(error => console.error(error)); /* handle errors */
@@ -110,13 +90,15 @@ $("#addMovie").click(() => {
     }
 });
 
+$(document).on("click", ".edit", function() {
+    IDToEdit = $(this).parent().attr('id');
+    console.log(IDToEdit);
+});
+
 $(document).on("click", ".submit-edit", function(){
-    let editID = $(this).parent().parent().parent().attr('id');
-    editID = editID.substring(8);
-    console.log(editID);
-    let editRating = $(`input[name="editRating${editID}"]:checked`).val();
+    let editRating = $(`input[name="editRating"]:checked`).val();
     console.log(editRating)
-    moviePatch(editID, editRating);
+    moviePatch(editRating);
 });
 
 $(document).on("click", ".delete", function(){
